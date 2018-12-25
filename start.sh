9be05b31a2ca5891ac71221b1cd4c6d15d09500f
Mohammed Rajab <moh.rajab@dce.sa>
2018-12-25 12:24:08 +0200

    update start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# create storage folders structure
+cd /var/www/storage
+mkdir -p app/public
+mkdir -p framework/cache
+mkdir -p framework/sessions
+mkdir -p framework/testing
+mkdir -p framework/views
+
+chmod -R 777 /var/www/storage
+
+
+# get files from remote server and add it to volume
+wget  -O /var/www/public/ "http://fleet.toyota.com.sa/uploads.zip"
+unzip uploads.zip
